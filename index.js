f9ce5ece06c1af2fd08c9e335974e7c8247a0c71
Guilherme da Silva Benevides <gsbenevides2@gmail.com>
2023-07-13 17:29:12 -0300

    fix: server port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 server.use(router)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 server.listen(port, () => {
   console.log('JSON Server is running')
