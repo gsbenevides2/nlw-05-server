8f413937e1a3d9c49664a6a6b9219b92db8fd450
Guilherme da Silva Benevides <gsbenevides2@gmail.com>
2023-08-09 09:11:42 -0300

    feat: use docker compose

--- run_container.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run -d \
-    -p 3001:8080 \
-    --name nlw-05-server \
-    --restart=always \
-    nlw-05-server
---
+++ up-compose.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker compose -p nlw-05-server -f docker-compose.yml up -d
